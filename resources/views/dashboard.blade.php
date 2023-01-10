b88cae68e650ae009da2e073e261dbcbc53caa7d
Muhammad Rafi Andeo Praja <76136679+RafiAndeo@users.noreply.github.com>
2023-01-10 16:29:26 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,13 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+                <div class="h-10 mt-4">
+                    <a
+                        href="{{ route('index') }}"class="bg-green-500 rounded-full px-4 py-2 mx-4 my-4 text-xs font-semibold text-white">
+                        <- Menuju Halaman Utama ->
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
